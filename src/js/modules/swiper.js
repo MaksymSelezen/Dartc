b2d01a9123316291ddff00fa336bef54af5d18e6
MaksymSelezen <foqney@gmail.com>
2026-04-18 23:55:41 +0300

    section hero / promo-banner / products / featured-product / contact

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -5,10 +5,11 @@ const heroSlider = document.querySelector(".hero__slider");
 
 if (heroSlider) {
   new Swiper(heroSlider, {
-    slidesPerView: 1.2,
+    slidesPerView: "auto",
     spaceBetween: 15,
     speed: 500,
     grabCursor: true,
+    watchOverflow: true,
     loop: true,
   });
 }
